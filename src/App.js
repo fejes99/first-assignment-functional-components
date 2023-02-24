3c2aef0641daaee883c061c15c680e9f623d99c9
David Fejes <35314157+fejes99@users.noreply.github.com>
2023-02-24 10:31:11 +0100

    Transforming class based components to functional components

--- src/App.js
+++ src/App.js
@@ -6,19 +6,17 @@ import Calculator from './components/calculator/Calculator';
 import UsersTable from './components/UsersTable/UsersTable';
 import Welcome from './components/Welcome/Welcome';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Navbar />
-        <Switch>
-          <Route path='/calculator' component={Calculator} />
-          <Route path='/users' component={UsersTable} />
-          <Route path='/' component={Welcome} />
-        </Switch>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Switch>
+        <Route path='/calculator' component={Calculator} />
+        <Route path='/users' component={UsersTable} />
+        <Route path='/' component={Welcome} />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
